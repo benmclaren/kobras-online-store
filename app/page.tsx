e1d9708d2e5ca0f2ffbba0b96e11d707b8168428
Ben McLaren <benmclaren@live.co.uk>
2023-05-31 17:45:48 +0100

    added strip products

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,37 @@
+import { log } from "console"
+import Stripe from "stripe"
+import Product from "./components/Product"
 
-export default function Home() {
+const getProducts = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2022-11-15",
+  })
+  const products = await stripe.products.list()
+  console.log(products)
+
+  const productsWithPrices = await Promise.all(
+    products.data.map(async (product) => {
+      const prices = await stripe.prices.list({product: product.id})
+      return{
+        id: product.id,
+        name: product.name,
+        price: prices.data[0].unit_amount,
+        image: product.images[0],
+        currency: prices.data[0].currency
+      }
+    })
+  )
+  return productsWithPrices
+}
+
+export default async function Home() {
+  const products = await getProducts()
+  console.log(products)
   return (
    <main>
-    <h1 className="text-4xl">Hello Next 13 🔥</h1>
+    {products.map((product) => (
+      <Product />
+    ))}
    </main>
   )
 }
