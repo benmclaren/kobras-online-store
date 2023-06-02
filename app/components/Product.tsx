d767ce4de708cf495502e39333fda0d19d19ba1f
Ben McLaren <benmclaren@live.co.uk>
2023-06-02 16:29:07 +0100

    added additional description page styling

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -5,14 +5,16 @@ import Link from "next/link"
 import { link } from "fs"
 
 //  ProductType specifies type of data each arguemtn should be
-export default function Product({name, price, image, id}: ProductType){
+export default function Product({name, image, unit_amount, id, description, metadata}: ProductType){
+  // extracting the features content from metadata
+  const { features } = metadata
   return(
-    <Link href={{pathname: `/products/${id}`, query: { name, image, price, id }}}>
+    <Link href={{pathname: `/products/${id}`, query: { name, image, unit_amount, id, description, features }}}>
       <div className="text-gray-700">
         <Image src={image} alt={name} width={800} height={800} className="h-96 w-full object-cover rounded-lg"/>
         <div className="font-medium py-2">
           <h1>{name}</h1>
-          <h2 className="text-sm text-teal-700">{price !== null ? formatPrice(price) : "N/A"}</h2>
+          <h2 className="text-sm text-teal-700">{unit_amount !== null ? formatPrice(unit_amount) : "N/A"}</h2>
         </div>
       </div>
     </Link>
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import { SearchParamTypes } from "@/types/SearchParamTypes";
+import formatPrice from "@/util/PriceFormat";
 
-export default async function Product({searchParams}){
+export default async function Product({searchParams}: SearchParamTypes){
   console.log(searchParams)
   return(
     <div className="flex justify-between gap-24 p-12 text-gray-700">
@@ -10,9 +12,13 @@ export default async function Product({searchParams}){
         width={600} 
         height={600}
       />
-      <div>
-        <h1>{searchParams.name}</h1>
-        <p>{searchParams.price}</p>
+      <div className="text=gray-700 font-medium">
+        <h1 className="text-2xl font-medium py-2">{searchParams.name}</h1>
+        <p className="py-2">{searchParams.description}</p>
+        <div className="flex gap-2">
+          <p className="font-bold text-teal-700">{searchParams.unit_amount && formatPrice(searchParams.unit_amount)}</p>
+        </div>
+        <button className="my-12 text-white py-2 px-6 font-medium rounded bg-teal-700">Add to cart</button>
       </div>
     </div>
   )
