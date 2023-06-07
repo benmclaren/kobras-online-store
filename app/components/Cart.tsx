4c98888af563064c1a901afafc49d6fc35d84025
Ben McLaren <benmclaren@live.co.uk>
2023-06-07 17:18:34 +0100

    connected add and remove functions

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image"
 import { useCartStore } from "@/store"
 import formatPrice from "@/util/PriceFormat"
+import {IoAddCircle, IoRemoveCircle} from 'react-icons/io5'
 
 // The physical cart UI
 export default function Cart() {
@@ -29,7 +30,35 @@ export default function Cart() {
             />
             <div>
               <h2>{item.name}</h2>
-              <h2>x {item.quantity}</h2>
+              <div className="flex gap-2">
+                <button 
+                  onClick={() => 
+                    cartStore.removeProduct({
+                      id: item.id, 
+                      image: item.image, 
+                      name: item.name, 
+                      unit_amount: item.unit_amount, 
+                      quantity: item.quantity
+                    })
+                  }
+                >
+                  <IoRemoveCircle />
+                </button>
+                <h2>{item.quantity}</h2>
+                <button 
+                  onClick={() => 
+                    cartStore.addProduct({
+                      id: item.id, 
+                      image: item.image, 
+                      name: item.name, 
+                      unit_amount: item.unit_amount, 
+                      quantity: item.quantity
+                    })
+                  }
+                >
+                  <IoAddCircle />
+                </button>
+              </div>
               <p className="text-sm">{item.unit_amount && formatPrice(item.unit_amount)}</p>
             </div>
           </div>
