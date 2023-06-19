7f8b3279eabd78ed0416244914360e489996c1c3
Ben McLaren <benmclaren@live.co.uk>
2023-06-19 12:44:24 +0100

    order api call refactored

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,30 @@
 // to fetch and display the orders from the user
-export default function Dashboard(){
+
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+
+const fetchOrders = async () => {
+  const prisma = new PrismaClient()
+  const user = await getServerSession(authOptions)
+  if(!user) {
+   return {message: 'Not logged in'}
+  }
+  const orders = await prisma.order.findMany({
+    where: {
+      userId: user?.user?.id
+    },
+    include: {
+      products: true
+    }
+  })
+  return orders
+}
+
+export default async function Dashboard(){
+  const orders = await fetchOrders()
+  console.log(orders)
   return(
     <div>
       <h1>Dashboard</h1>
