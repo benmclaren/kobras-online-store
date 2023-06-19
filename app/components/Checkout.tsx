598d61e4703c17c59109f672c61e9fead8c1c173
Ben McLaren <benmclaren@live.co.uk>
2023-06-19 17:05:29 +0100

    added framer motion to the pay now btn

--- app/components/Checkout.tsx
+++ app/components/Checkout.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import CheckoutForm  from './CheckoutForm'
 import OrderAnimation from './OrderAnimation'
+import { motion } from 'framer-motion'
 
 // need the NEXT_PUBLIC as we are rendering on client side.Not needed when server component
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
@@ -53,11 +54,11 @@ const options: StripeElementsOptions = {
     <div>
       {!clientSecret && <OrderAnimation/>}
       {  clientSecret && (
-        <div>
+        <motion.div initial={{opacity: 0}} animate={{opacity: 1}}>
           <Elements options={options} stripe={stripePromise}>
             <CheckoutForm clientSecret={clientSecret}/>
           </Elements>
-        </div>
+        </motion.div>
       )}
     </div>
   )
