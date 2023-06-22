6c4d3363469f8a5461cd8ea87e085fb166be7813
Ben McLaren <benmclaren@live.co.uk>
2023-06-22 11:51:05 +0100

    bug fix for items not adding to cart in production

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -8,8 +8,9 @@ import { link } from "fs"
 export default function Product({name, image, unit_amount, id, description, metadata}: ProductType){
   // extracting the features content from metadata
   const { features } = metadata
+  const queryId = id
   return(
-    <Link href={{pathname: `/products/${id}`, query: { name, image, unit_amount, id, description, features }}}>
+    <Link href={{pathname: `/products/${id}`, query: { name, image, unit_amount, queryId, description, features }}}>
       <div className="text-gray-700">
         <Image src={image} alt={name} width={800} height={800} className="h-96 w-full object-cover rounded-lg"/>
         <div className="font-medium py-2">
