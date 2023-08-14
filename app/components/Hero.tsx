6d12959dc9f0587906132acb4ab2e7f752e55a41
Ben McLaren <benmclaren@live.co.uk>
2023-08-14 12:12:00 +0100

    replace banner

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import Image from "next/image"
-import kobrasbanner from '@/public/kobrasbanner.png'
+import kobrabannertext from '/public/kobrabannertext.png'
 
 
 export default function Hero() {
   return(
-    <Image src={kobrasbanner} alt="banner image of outgoing kobras" className="py-8 block"/>
+      <div className="h-100">
+        <img src="/kobrabannertext.png" alt="" />
+      </div>
+  
   )
 }
