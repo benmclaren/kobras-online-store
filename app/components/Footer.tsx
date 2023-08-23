7d86c318d6471b18e63a873ee1a6e37ade0c658d
Ben McLaren <benmclaren@live.co.uk>
2023-08-23 17:45:04 +0100

    added footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link"
+
 export default function Footer() {
   return(
-    <h1>footer</h1>
+    <div className="bg-slate-800 flex justify-between p-4 font-nohemi mt-8">
+      <div className="flex flex-col">
+        <Link href={"/products"} className="hover:text-color-secondary transition duration-75">Products</Link>
+        <Link href={"/dashboard"} className="hover:text-color-secondary transition duration-75">Orders</Link>
+        <h4 className="font-medium mt-2">©️Outgoing Kobras Online Store 2023</h4>
+      </div>
+      <div className="w-64">
+        <p>Outgoing Kobras FC store is not currently a real store.</p>
+        <p className="mt-2">Made with love by <Link href={"https://benmclaren.xyz/"} className="text-color-secondary hover:underline transition duration-75">Ben McLaren</Link></p>
+      </div>
+    </div>
   )
 }
